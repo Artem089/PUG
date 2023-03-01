e42924d7ae41b853a78a3b8e1ca8bef06684cfa5
Artem089 <64342391+Artem089@users.noreply.github.com>
2023-03-01 14:07:29 +0300

    Add files via upload

---
+++ pug/app.js
@@ -0,0 +1,26 @@
+const express = require("express");
+  
+const app = express();
+  
+app.set("view engine", "pug");
+ 
+app.use("/login", function(request, response){
+    response.render("login");
+}); 
+
+app.use("/register", function(request, response){
+    response.render("register");
+}); 
+
+app.use("/index", function(request, response){
+    response.render("index");
+}); 
+ 
+app.use(express.static('public'))
+
+app.use("/", function(request, response){
+      
+    response.send("Главная страница");
+});
+
+app.listen(3000);   
